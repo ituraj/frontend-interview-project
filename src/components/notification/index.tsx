ffd4e459338dac9343cf603082ec6c31a284a60e
Izabela Turaj <i.turaj@gmail.com>
2020-08-05 21:59:01 +0200

    improved animation and auto hide notification

--- src/components/notification/index.tsx
+++ src/components/notification/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import classnames from 'classnames';
 import styles from './notification.module.scss';
 import Icon from '../icon';
@@ -7,24 +7,45 @@ interface Props {
   className?: Optional<string>;
   color?: string;
   iconKey?: string;
+  mode?: string;
 }
 
 const Notification: FC<Props> = (props) => {
-  const { className, color, iconKey, children, ...otherProps } = props;
+  const { className, color, iconKey, mode, children, ...otherProps } = props;
+  const [open, setOpen] = useState(true);
+
+  const handleClose = (): void => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (mode === 'auto-hide') {
+        setOpen(false);
+      }
+    }, 3000);
+    return (): void => {
+      clearTimeout(timeout);
+    };
+  }, [mode]);
 
   const rootClass = classnames(
     {
-      [styles.root]: true,
+      [styles.notification]: true,
+      [styles.notificationShow]: open,
+      [styles.notificationHide]: !open,
     },
     className,
   );
 
   const containerStyle: Record<string, unknown> = {
-    borderTopColor: undefined,
+    borderLeftColor: undefined,
+    color: undefined,
   };
 
   if (color && color.length) {
     containerStyle.borderLeftColor = color;
+    containerStyle.color = color;
   }
 
   return (
@@ -32,7 +53,11 @@ const Notification: FC<Props> = (props) => {
       <div className={styles.container} style={containerStyle}>
         {!!iconKey && <Icon name={iconKey} className={styles.icon} />}
         <div className={styles.message}>{children}</div>
-        <Icon name="times" className={styles.close} />
+        <button className={styles.btn} onClick={handleClose}>
+          {mode !== 'auto-hide' && (
+            <Icon name="times" className={styles.close} />
+          )}
+        </button>
       </div>
     </div>
   );
--- src/stories/3-Notification.stories.tsx
+++ src/stories/3-Notification.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { text } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import Notification from '../components/notification';
 
 const notification = {
@@ -19,6 +19,10 @@ const notification = {
     title: 'Something is wrong! :(',
     description: 'This is an error notification.',
   },
+  autoHide: {
+    title: 'Give it a few seconds!',
+    description: 'This notification will hide.',
+  },
 };
 
 const titleStyle: Record<string, unknown> = {
@@ -27,6 +31,7 @@ const titleStyle: Record<string, unknown> = {
   letterSpacing: 'normal',
   margin: '0',
   marginBottom: '0.2rem',
+  color: '#191919',
 };
 
 const descriptionStyle: Record<string, unknown> = {
@@ -34,47 +39,52 @@ const descriptionStyle: Record<string, unknown> = {
   fontSize: '12px',
   letterSpacing: '0.3px',
   margin: '0',
+  color: '#191919',
 };
 
 export const info: React.FC = () => (
-  <div>
-    <Notification color={text('color', '#0183fd')} iconKey="info-circle">
-      <h4 style={titleStyle}>{notification.info.title}</h4>
-      <p style={descriptionStyle}>{notification.info.description}</p>
-    </Notification>
-  </div>
+  <Notification color={text('color info', '#0183fd')} iconKey="info-circle">
+    <h4 style={titleStyle}>{notification.info.title}</h4>
+    <p style={descriptionStyle}>{notification.info.description}</p>
+  </Notification>
 );
 
 export const success: React.FC = () => (
-  <div>
-    <Notification color={text('color2', '#0aaf7a')} iconKey="check-circle">
-      <h4 style={titleStyle}>{notification.success.title}</h4>
-      <p style={descriptionStyle}>{notification.success.description}</p>
-    </Notification>
-  </div>
+  <Notification color={text('color success', '#0aaf7a')} iconKey="check-circle">
+    <h4 style={titleStyle}>{notification.success.title}</h4>
+    <p style={descriptionStyle}>{notification.success.description}</p>
+  </Notification>
 );
 
 export const warning: React.FC = () => (
-  <div>
-    <Notification
-      color={text('color3', '#fe8e00')}
-      iconKey="exclamation-circle"
-    >
-      <h4 style={titleStyle}>{notification.warning.title}</h4>
-      <p style={descriptionStyle}>{notification.warning.description}</p>
-    </Notification>
-  </div>
+  <Notification
+    color={text('color warning', '#fe8e00')}
+    iconKey="exclamation-circle"
+  >
+    <h4 style={titleStyle}>{notification.warning.title}</h4>
+    <p style={descriptionStyle}>{notification.warning.description}</p>
+  </Notification>
 );
 
 export const error: React.FC = () => (
-  <div>
-    <Notification color={text('color4', '#dc2626')} iconKey="times-circle">
-      <h4 style={titleStyle}>{notification.error.title}</h4>
-      <p style={descriptionStyle}>{notification.error.description}</p>
-    </Notification>
-  </div>
+  <Notification color={text('color error', '#dc2626')} iconKey="times-circle">
+    <h4 style={titleStyle}>{notification.error.title}</h4>
+    <p style={descriptionStyle}>{notification.error.description}</p>
+  </Notification>
+);
+
+export const autoHide: React.FC = () => (
+  <Notification
+    color={text('color regular', '#5e5d5e')}
+    iconKey="pause-circle"
+    mode={text('mode', 'auto-hide')}
+  >
+    <h4 style={titleStyle}>{notification.autoHide.title}</h4>
+    <p style={descriptionStyle}>{notification.autoHide.description}</p>
+  </Notification>
 );
 
 export default {
   title: 'Notification',
+  decorators: [withKnobs],
 };
